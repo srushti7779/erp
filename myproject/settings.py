13c37f94d9dbaf4f92c78deff97b99136f2e73f8
KartikInfynno <kartik.infynno@gmail.com>
2023-05-25 01:45:01 +0530

    added allowed hosts

--- myproject/settings.py
+++ myproject/settings.py
@@ -131,5 +131,9 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
+    "http://153.92.5.222:8000"
+]
 
