fffaa0e3ce6a13a71169312d5cc6ac8adf847f0d
KartikInfynno <kartik.infynno@gmail.com>
2023-05-25 22:57:43 +0530

    reverted changes

---
+++ app/static/app/js/main.js
@@ -0,0 +1,162 @@
+jQuery(function ($) {
+    $(".nav-item a")
+        .click(function(e) {
+            var link = $(this);
+
+            var item = link.parent("li");
+            
+            if (item.hasClass("active1")) {
+                item.removeClass("active1").children("a").removeClass("active1");
+            } else {
+                item.addClass("active1").children("a").addClass("active1");
+            }
+
+            if (item.children("ul").length > 0) {
+                var href = link.attr("href");
+                link.attr("href", "#");
+                setTimeout(function () { 
+                    link.attr("href", href);
+                }, 300);
+                e.preventDefault();
+            }
+        })
+        .each(function() {
+            var link = $(this);
+            if (link.get(0).href === location.href) {
+                link.addClass("active1").parents("li").addClass("active1");
+                return false;
+            }
+        });
+});
+
+
+
+jQuery(function ($) {
+    $(".menu .nav-item a")
+        .click(function(e) {
+            var link = $(this);
+
+            var item = link.parent("li");
+            
+            if (item.hasClass("active1")) {
+                item.removeClass("active1").children("a").removeClass("active1");
+            } else {
+                item.addClass("active1").children("a").addClass("active1");
+            }
+
+            if (item.children("ul").length > 0) {
+                var href = link.attr("href");
+                link.attr("href", "#");
+                setTimeout(function () { 
+                    link.attr("href", href);
+                }, 300);
+                e.preventDefault();
+            }
+        })
+        .each(function() {
+            var link = $(this);
+            if (link.get(0).href === location.href) {
+                link.addClass("active1").parents("li").addClass("active1");
+                return false;
+            }
+        });
+});
+
+
+
+$( document ).ready(function() {
+
+    $( ".cross" ).hide();
+    $( ".menu" ).hide();
+    $( ".hamburger" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".hamburger" ).hide();
+    $( ".cross" ).show();
+    });
+    });
+    
+    $( ".cross" ).click(function() {
+    $( ".menu" ).slideToggle( "slow", function() {
+    $( ".cross" ).hide();
+    $( ".hamburger" ).show();
+    });
+    });
+    
+});
+
+
+// debounce from underscore.js
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
+// use x and y mousewheel event data to navigate flickity
+function slick_handle_wheel_event(e, slick_instance, slick_is_animating) {
+  // do not trigger a slide change if another is being animated
+  if (!slick_is_animating) {
+    // pick the larger of the two delta magnitudes (x or y) to determine nav direction
+    var direction =
+      Math.abs(e.deltaX) > Math.abs(e.deltaY) ? e.deltaX : e.deltaY;
+
+    console.log("wheel scroll ", e.deltaX, e.deltaY, direction);
+
+    if (direction > 0) {
+      // next slide
+      slick_instance.slick("slickNext");
+    } else {
+      // prev slide
+      slick_instance.slick("slickPrev");
+    }
+  }
+}
+
+// debounce the wheel event handling since trackpads can have a lot of inertia
+var slick_handle_wheel_event_debounced = debounce( 
+  slick_handle_wheel_event
+  , 100, true
+);
+
+// init slider 
+const slick_2 = $(".slides");
+slick_2.slick({
+  vertical: true,
+  verticalSwiping: true,
+  arrows: false
+});
+var slick_2_is_animating = false;
+
+slick_2.on("afterChange", function(index) {
+  console.log("Slide after change " + index);
+  slick_2_is_animating = false;
+});
+
+slick_2.on("beforeChange", function(index) {
+  console.log("Slide before change " + index);
+  slick_2_is_animating = true;
+});
+
+slick_2.on("wheel", function(e) {
+  slick_handle_wheel_event_debounced(e.originalEvent, slick_2, slick_2_is_animating);  
+});
+
+
+$('[data-fancybox="gallery"]').fancybox({
+  baseClass : 'fancybox-custom-layout',
+  buttons: [
+   
+    "close",
+  ],
+  loop: false,
+  protect: true
+});
--- myproject/settings.py
+++ myproject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-l#5l$t)o9!(288sgj+n3mzi1v-=*5j=@*0)ktqi=72^p7gt(2f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1' ,'153.92.5.222', 'erpdesignstudio.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app',
-    "corsheaders",
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -50,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
 ]
 
 ROOT_URLCONF = 'myproject.urls'
@@ -58,7 +56,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR / 'templates' ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,19 +117,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8080",
-    "http://127.0.0.1:9000",
-    "http://153.92.5.222:8000"
-]
-
