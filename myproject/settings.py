a191d6e064ffb05bccc736202e88d9937bc923c1
KartikInfynno <kartik.infynno@gmail.com>
2023-05-26 00:19:34 +0530

    .

--- myproject/settings.py
+++ myproject/settings.py
@@ -81,12 +81,13 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-
-MEDIA_URL = '/media/'
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = '/media/'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
